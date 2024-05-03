8ae34ac14f4c79d814d002dc3889d61ed102947f
Tejas Kumar S <tej0897@gmail.com>
2024-05-03 13:39:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,16 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  // const inputs = [];
-  // const quotients = [];
-  // const remainders = [];
+  let binary = "";
 
-  // if (input === 0) {
-  //   result.innerText = "0";
-  //   return;
-  // }
 
-  // while (input > 0) {
-  //   const quotient = Math.floor(input / 2);
-  //   const remainder = input % 2;
 
-  //   inputs.push(input);
-  //   quotients.push(quotient);
-  //   remainders.push(remainder);
-  //   input = quotient;
-  // }
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
+  }
 
-  // console.log("Inputs: ", inputs);
-  // console.log("Quotients: ", quotients);
-  // console.log("Remainders: ", remainders);
-
-  // result.innerText = remainders.reverse().join("");
-  
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {

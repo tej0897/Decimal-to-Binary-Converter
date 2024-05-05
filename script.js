44ebe06b5440d5031e64975a0da18b34682dfb0a
Tejas Kumar S <tej0897@gmail.com>
2024-05-05 11:20:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,12 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const countDownAndUp = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-
-    return;
-  } else {
-    countDownAndUp(number - 1);
-  }
-};
-
-countDownAndUp(3);
-
 const decimalToBinary = (input) => {
-  let binary = "";
-
   if (input === 0) {
-    binary = "0";
+    return "";
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input %2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {

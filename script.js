a5bbc035ed196d6298c6eb06908d50a7d29dd380
Tejas Kumar S <tej0897@gmail.com>
2024-05-05 13:16:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,26 @@ const decimalToBinary = (input) => {
   }
 };
 
-
+const showAnimation = () => {
+  console.log("free");
+  console.log("Code");
+  console.log("Camp");
+};
 
 const checkUserInput = () => {
-  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt)) {
     alert("Please provide a decimal number");
     return;
   }
 
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 
